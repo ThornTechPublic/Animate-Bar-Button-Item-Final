b96f1493b948693e0f6e5637d0adb6da92c87e54
Robert Chen <robert.chen@thorntechnologies.com>
2015-08-16 11:42:47 -0400

    Setting up the right button

--- animateBarButtonItem/ViewController.swift
+++ animateBarButtonItem/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func starTapped(sender: AnyObject) {
+        println("star tapped")
+    }
 
 }
 
